b9f0b15c91707953619c24a88f844a688096cd63
Ivanko Mike <spbima@mail.ru>
2023-07-05 16:00:08 +0300

    Added step 4

---
+++ step4/src/main/java/ru/devinfotools/spring/TestSpring.java
@@ -0,0 +1,17 @@
+package ru.devinfotools.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        context.close();
+    }
+}
